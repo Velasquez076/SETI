d327635e6685efd432f88dd3d017963f4ecdbd0c
Julián Velásquez <velasquezjulian914@gmail.com>
2025-05-17 17:51:25 -0500

    Update object response franchise

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/FranchiseDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/FranchiseDto.java
@@ -2,6 +2,6 @@ package co.com.bancolombia.api.dto;
 
 import java.util.UUID;
 
-public record FranchiseDto(Long id, String name, UUID uuid) {
+public record FranchiseDto(Long id, String name) {
 
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/franchise/FranchiseHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/franchise/FranchiseHandler.java
@@ -1,11 +1,13 @@
 package co.com.bancolombia.api.franchise;
 
 import co.com.bancolombia.api.dto.FranchiseDto;
+import co.com.bancolombia.api.dto.WrapperResponse;
 import co.com.bancolombia.model.franchise.Franchise;
 import co.com.bancolombia.usecase.franchise.FranchiseUseCase;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -24,7 +26,7 @@ public class FranchiseHandler {
         .map(franchiseDto -> new Franchise(franchiseDto.id(), franchiseDto.name()))
         .flatMap(franchiseUseCase::save)
         .doOnNext(f -> log.info("Saved franchise: id={}, name={}", f.getId(), f.getName()))
-        .map(saved -> new FranchiseDto(saved.getId(), saved.getName(), UUID.randomUUID()))
+        .map(franchise -> buildWrapper(franchise, 201))
         .flatMap(dto -> ServerResponse.ok().bodyValue(dto))
         .doOnSuccess(sr -> log.info("Franchise saved successfully! {}", sr.statusCode()));
   }
@@ -33,8 +35,13 @@ public class FranchiseHandler {
     return serverRequest.bodyToMono(FranchiseDto.class)
         .map(franchiseDto -> new Franchise(franchiseDto.id(), franchiseDto.name()))
         .flatMap(franchiseUseCase::update)
-        .map(saved -> new FranchiseDto(saved.getId(), saved.getName(), UUID.randomUUID()))
+        .map(franchise -> buildWrapper(franchise, 200))
         .flatMap(dto -> ServerResponse.ok().bodyValue(dto))
         .doOnSuccess(sr -> log.info("Franchise updated successfully! {}", sr.statusCode()));
+  }
+
+  private WrapperResponse<FranchiseDto> buildWrapper(Franchise franchise, int code) {
+    return new WrapperResponse<>(UUID.randomUUID(), HttpStatusCode.valueOf(code),
+        new FranchiseDto(franchise.getId(), franchise.getName()));
   }
 }
